fcbe8147e9af8421238ebda0eb8caead7db0a9dd
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-31 11:58:54 -0700

    Add get tests to Event class.

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -11,6 +11,45 @@
 
     class EventTest extends PHPUnit_Framework_TestCase
     {
+
+        function test_getId()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event= new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $result = $test_event->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
+
+        }
+
+        function test_getDate()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $result = $test_event->getDate();
+
+            //Assert
+            $this->assertEquals('2015-10-01 12:24:55', $result);
+
+        }
+
         function test_getEventName()
         {
             //Arrange
@@ -26,8 +65,50 @@
             $result = $test_event->getEventName();
 
             //Assert
-            $this->assertEquals($event_name, $result);
+            $this->assertEquals("Portland Marathon", $result);
         }
+
+        function test_getLocation()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $result = $test_event->getLocation();
+
+            //Assert
+            $this->assertEquals("Downtown Portland", $result);
+
+        }
+
+        function test_getUserId()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $result = $test_event->getUserId();
+
+            //Assert
+            $this->assertEquals(2, $result);
+
+        }
+
+        
+
+
 
     }//Ends class
 
