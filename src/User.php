a081093b1a816bdfa99b20c80ae62c1267a133d1
David Eliason & Kyle Giard-Chase <student@epicodus.com>
2015-03-31 10:53:07 -0700

    Add getters and setters

--- src/User.php
+++ src/User.php
@@ -2,7 +2,59 @@
 
     class User
     {
-        
+        private $name;
+        private $email;
+        private $password;
+        private $id;
+
+        function __construct($name, $email, $password, $id = null)
+        {
+            $this->name = $name;
+            $this->email = $email;
+            $this->password = $password;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function setEmail($new_email)
+        {
+            $this->email = (string) $new_email;
+        }
+
+        function getEmail()
+        {
+            return $this->email;
+        }
+
+        function setPassword($new_password)
+        {
+            $this->password = (string) $new_password
+        }
+
+        function getPassword()
+        {
+            return $this->password;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setId($new_id)
+        {
+            $this->id = $new_id;
+        }
+
     }
 
 ?>
