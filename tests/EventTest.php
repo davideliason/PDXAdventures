5789782723334c9ce588ca2c33039a68550d910c
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-31 11:27:01 -0700

    Add passing test for getEventName and git workflow.txt.

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -7,11 +7,27 @@
 
     require_once "src/Event.php";
 
-    $DB = new PDO('pgsql:host=localhost;dbname=adventure_test');
+    //$DB = new PDO('pgsql:host=localhost;dbname=adventure_test');
 
     class EventTest extends PHPUnit_Framework_TestCase
     {
-        function test_get
+        function test_getEventName()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $result = $test_event->getEventName();
+
+            //Assert
+            $this->assertEquals($event_name, $result);
+        }
 
     }//Ends class
 
