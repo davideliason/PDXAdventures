b1a29eb9c7787e75360633df132b832c8984180d
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-31 15:48:06 -0700

    Add addActivites test.

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -287,6 +287,24 @@
             $this->assertEquals([], $result);
         }
 
+        function testAddActivity()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+            $test_event->save();
+
+            $test_activity = new Activity($id, $activity_name);
+
+            //Act
+
+        }
+
 
     }//Ends class
 
