05f3de8fabd75afd8bd357c9594ca8c759fd5ede
Bojana Skarich <bojana.skarich@gmail.com>
2015-03-31 12:12:24 -0700

    Add passing tests for Event class setters.

--- tests/EventTest.php
+++ tests/EventTest.php
@@ -12,6 +12,7 @@
     class EventTest extends PHPUnit_Framework_TestCase
     {
 
+        //SETTERS
         function test_getId()
         {
             //Arrange
@@ -105,8 +106,106 @@
             $this->assertEquals(2, $result);
 
         }
+        //SETTERS
+        function test_setId()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event= new Event ($id, $date, $description, $event_name, $location, $user_id);
 
-        
+            //Act
+            $test_event->setId(2);
+
+            //Assert
+            $result = $test_event->getId();
+            $this->assertEquals(2, $result);
+
+        }
+
+        function test_setDate()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $test_event->setDate('2016-11-01 12:24:55');
+
+            //Assert
+            $result = $test_event->getDate();
+            $this->assertEquals('2016-11-01 12:24:55', $result);
+
+        }
+
+        function test_setEventName()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $test_event->setEventName("Mt Tabor Soap Box Derby");
+
+            //Assert
+            $result = $test_event->getEventName();
+            $this->assertEquals("Mt Tabor Soap Box Derby", $result);
+        }
+
+        function test_setLocation()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $test_event->setLocation("Hillsboro");
+
+            //Assert
+            $result = $test_event->getLocation();
+            $this->assertEquals("Hillsboro", $result);
+
+        }
+
+        function test_setUserId()
+        {
+            //Arrange
+            $id = 1;
+            $date = '2015-10-01 12:24:55';
+            $description = "26.5 miles of fun";
+            $event_name = "Portland Marathon";
+            $location = "Downtown Portland";
+            $user_id = 2;
+            $test_event = new Event ($id, $date, $description, $event_name, $location, $user_id);
+
+            //Act
+            $test_event->setUserId(3);
+
+            //Assert
+            $result = $test_event->getUserId();
+            $this->assertEquals(3, $result);
+
+        }
+
 
 
 
